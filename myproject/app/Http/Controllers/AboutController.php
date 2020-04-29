814baa5f8381903d5f7551f447f094388513f3c0
名前 <>
2020-04-29 04:48:48 +0000

    originalを作り直し挑戦開始

---
+++ myproject/app/Http/Controllers/AboutController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+    public function add()
+  {
+      return view('about');
+  }
+  
+}
